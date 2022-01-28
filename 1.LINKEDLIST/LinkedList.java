889be635526eb5f41717697b7089925fc0e02d1d
HEALER <65026164+healer-op@users.noreply.github.com>
2022-01-28 13:07:55 +0530

    Update LinkedList.java

--- 1.LINKEDLIST/LinkedList.java
+++ 1.LINKEDLIST/LinkedList.java
@@ -1,5 +1,7 @@
+package com.aditya;
+
 import java.util.Scanner;
-public class InsertAtBegLL {
+public class LinkedList {
 
     static class Node{
         int data;
@@ -12,50 +14,95 @@ public class InsertAtBegLL {
     Node head = null; //pointer
 
     public void creation(){
-        int data,n,m,pos;
+        int data,m=1,pos;
         Scanner sc = new Scanner(System.in);
-        do{
+        while(m!=-1) {
             System.out.println("Enter data");
-            data=sc.nextInt();
-            Node new_node=new Node(data);
-            if(head==null){
-                head=new_node; // 10
-            }
-            else{
-                System.out.println("Enter 1 to insert the insert the item at Beginning");
-                System.out.println("Enter 2 to insert the insert the item at End");
-                System.out.println("Enter 3 to insert the insert the item at Particular Location");
-                m=sc.nextInt();
-                switch(m){
+            data = sc.nextInt();
+            Node new_node = new Node(data);
+            if (head == null) {
+                head = new_node; // 10
+            } else {
+                System.out.println("Enter 1 to insert  " +data+ " at Beginning");
+                System.out.println("Enter 2 to insert  " +data+ " at End");
+                System.out.println("Enter 3 to insert  " +data+ " at Particular Location");
+                System.out.println("Enter -1 to Exit");
+                m = sc.nextInt();
+                switch (m) {
                     case 1:
                         new_node.next = head;
                         head = new_node;
-                    break;
-                    
+                        break;
+
                     case 2:
                         Node temp = head;
-                        while(temp.next!=null){
-                            temp=temp.next;
+                        while (temp.next != null) {
+                            temp = temp.next;
                         }
-                        temp.next=new_node;
-                    break;
+                        temp.next = new_node;
+                        break;
 
                     case 3:
                         System.out.println("Enter The Pos");
                         pos = sc.nextInt();
                         Node temp1 = head;
-                        for(int i=0;i<(pos-1);i++){
-                            temp1=temp1.next;
+                        for (int i = 0; i < (pos - 1); i++) {
+                            temp1 = temp1.next;
                         }
-                        new_node.next=temp1.next;
-                        temp1.next=new_node;
-                    break;
+                        new_node.next = temp1.next;
+                        temp1.next = new_node;
+                        break;
                 }
             }
-            System.out.println("Do u want add more data if yes press 1");
-            n=sc.nextInt();
+
         }
-        while (n==1);
+        sc.close();
+    }
+
+    public void delete(){
+        int data,n,m,p;
+        Scanner sc =new Scanner(System.in);
+        do {
+            if (head == null) {
+                System.out.println("LL is empty");
+            } else {
+                System.out.println("");
+                System.out.println("Enter 1 to delete at Beginning");
+                System.out.println("Enter 2 to delete at End");
+                System.out.println("Enter 3 to delete at Particular Location");
+                m = sc.nextInt();
+                switch (m) {
+                    case 1:
+                        Node temp = head;
+                        temp = temp.next;
+                        head = temp;
+                        break;
+                    case 2:
+                        Node temp1 = head;
+                        Node ptr = temp1.next;
+                        while (ptr.next != null) {
+                            temp1 = ptr;
+                            ptr = ptr.next;
+                        }
+                        temp1.next = null;
+                        break;
+                    case 3:
+                        System.out.println("Enter The postion of node to be deleted");
+                        p = sc.nextInt();
+                        Node temp2 = head;
+                        Node ptr1 = temp2.next;
+                        for (int i = 0; i < p - 2; i++) {
+                            temp2 = ptr1;
+                            ptr1 = ptr1.next;
+                        }
+                        temp2.next = ptr1.next;
+                        break;
+                }
+            }
+            System.out.println("Do u want to delete more , press 1");
+            n = sc.nextInt();
+        }
+        while(n==1);
         sc.close();
     }
 
@@ -73,8 +120,9 @@ public class InsertAtBegLL {
     }
 
     public static void main(String[] args) {
-        InsertAtBegLL ll = new InsertAtBegLL();
+        LinkedList ll = new LinkedList();
         ll.creation();
+        ll.delete();
         ll.traverser();
     }
 }
